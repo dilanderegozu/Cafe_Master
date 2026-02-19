6747e6cad0712f31b8f87173529382d8057dd106
Dilan <dilanderegizu@icloud.com>
2026-02-19 15:45:12 +0300

    fixed

--- configs/redis.config.js
+++ configs/redis.config.js
@@ -1,16 +1,6 @@
 const Redis = require("ioredis");
 
-/** @type {import("ioredis").Redis} */
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  retryStrategy: (times) => {
-    const delay = Math.min(times * 50, 200);
-    return delay;
-  },
-});
-
+const redisClient = new Redis(process.env.REDIS_URL);
 redisClient.on("connect", () => {
   console.log("Redis bağlantısı başarılı");
 });
@@ -18,4 +8,5 @@ redisClient.on("connect", () => {
 redisClient.on("error", (err) => {
   console.log("Redis bağlantısı başarılı olamadı", err);
 });
+
 module.exports = redisClient;
